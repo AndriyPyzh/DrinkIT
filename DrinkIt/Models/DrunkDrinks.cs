d28c443d575d3d550a4919b3a8177fbf246bb86a
MartaKostetska <martakostetska1114@gmail.com>
2021-03-24 22:03:00 +0200

    added models

---
+++ DrinkIt/Models/DrunkDrinks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DrinkIt.Models
+{
+    public class DrunkDrinks
+    {
+        public DrunkDrinks()
+        {
+        }
+
+        public DrunkDrinks(int volume, DateTime now, Beverage beverage, User username)
+        {
+            this.Volume = volume;
+            this.Time = now;
+            this.Beverage = beverage;
+            this.User = username;
+        }
+        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        public int Volume { get; set; }
+        
+        public DateTime Time { get; set; }
+        
+        public Beverage Beverage { get; set; }
+        
+        public User User { get; set; } 
+    }
+}
---
+++ DrinkIt/Models/UserAchievements.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DrinkIt.Models
+{
+    public class UserAchievements
+    {
+        public int Id { get; set; }
+        
+        public User User { get; set; }
+        
+        public Achievement Achievement { get; set; }
+    }
+}
