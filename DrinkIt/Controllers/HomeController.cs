4229beba3ec81060bc33d85822e626a057738baf
AndriyPyzh <andriy.pyzh2412@gmail.com>
2021-04-13 23:45:37 +0300

    added ability to delete drunk drink

--- DrinkIt/Controllers/HomeController.cs
+++ DrinkIt/Controllers/HomeController.cs
@@ -23,34 +23,47 @@ namespace DrinkIt.Controllers
             _context.Dispose();
         }
 
-        public ActionResult Index(int page=1)
+        public ActionResult Index(int page = 1)
+        {
+            return View(GenerateModel(page));
+        }
+
+        public ActionResult DeleteDrunkDrink(int page, int id)
+        {
+            DrunkDrink drink = _context.DrunkDrinks.SingleOrDefault(x => x.Id == id);
+            _context.DrunkDrinks.Remove(drink);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home", new {page});
+        }
+
+        public HomeViewModel GenerateModel(int page)
         {
             String userId = User.Identity.GetUserId();
-            
+
             Account account = _context
                 .Users
                 .Include(u => u.Account.DrunkDrinks.Select(d => d.Beverage))
                 .Single(c => c.Id == userId)
                 .Account;
-            
+
             List<DrunkDrink> drinks = account
                 .DrunkDrinks
                 .Where(d => d.Time.Date == DateTime.Today)
                 .OrderByDescending(d => d.Time)
                 .ToList();
-            
+
             int intakeGoal = Convert.ToInt32(account.Goal);
-            
+
             int alreadyDrunked = drinks.Sum(d => d.Volume);
 
-            return View(new HomeViewModel
+            return new HomeViewModel
             {
                 BlogPerPage = 6,
                 CurrentPage = page,
                 Drinks = drinks,
                 IntakeGoal = intakeGoal,
                 AlreadyDrunk = alreadyDrunked
-            });
+            };
         }
     }
 }
