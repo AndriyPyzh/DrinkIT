f2c31165bd107f12e193687f1d7ebd7887f7ec77
Ігор Медвідь <Ihor.Medvid@lnu.edu.ua>
2021-04-29 15:32:11 +0300

    Revert "Revert "Added Validation""
    This reverts commit 69e31dbaaa0b4c86e8704fd74bff74b4496f31fa.

--- DrinkIt/Controllers/ProfileController.cs
+++ DrinkIt/Controllers/ProfileController.cs
@@ -30,25 +30,30 @@ namespace DrinkIt.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Profile(ProfileViewModel model)
         {
-            string userId = User.Identity.GetUserId();
-            ApplicationUser user = db.Users.Single(u => u.Id.Equals(userId));
-
-            Gender gender = db.Genders.Single(g => g.Name.Equals(model.Gender));
-            var profile = new Account
+            if (ModelState.IsValid)
             {
-                Age = model.Age,
-                Weight = model.Weight,
-                WakeUpTime = model.WakeUpTime,
-                SleepTime = model.SleepTime,
-                Goal = model.Goal,
-                Gender = gender,
-                WaterNorm = model.Weight * 30
-                
-            };
-            user.Account = profile;
-            db.Accounts.Add(profile);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index", "Home");
+                string userId = User.Identity.GetUserId();
+                ApplicationUser user = db.Users.Single(u => u.Id.Equals(userId));
+
+                Gender gender = db.Genders.Single(g => g.Name.Equals(model.Gender));
+                var profile = new Account
+                {
+                    Age = model.Age,
+                    Weight = model.Weight,
+                    WakeUpTime = new System.TimeSpan(0,0,8,0),
+                    SleepTime = new System.TimeSpan(0, 0, 22, 0),
+                    Goal = model.Goal,
+                    Gender = gender,
+                    WaterNorm = model.Weight * 30
+                    
+                   
+                };
+                user.Account = profile;
+                db.Accounts.Add(profile);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View("Index");
         }
         
     }
